f26ff9025343ae6a14b674ce002b1df9f6e8c7f4
Hassan Yahya <hassanz.2070@gmail.com>
2024-11-10 22:10:17 +0300

    Updated code to the latest version with our theme

--- script.js
+++ script.js
@@ -2,98 +2,34 @@ const scroll_indicator = document.getElementById("scroll-indicator");
 
 window.addEventListener("scroll", function () {
   const maxScrollHeight = document.body.scrollHeight - window.innerHeight;
-
   const currentScrollHeight = (window.scrollY / maxScrollHeight) * 100;
   scroll_indicator.style.width = `${currentScrollHeight}%`;
 });
 
-/* scroll reveal */
+/* Navbar Toggle */
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
 
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+
+/* ScrollReveal Animations */
 window.sr = ScrollReveal({
   reset: true,
 });
 
-sr.reveal(".hero .intro", {
-  duration: 2000,
-  origin: "left",
-  distance: "200px",
-});
-sr.reveal(".hero .intro .btn", {
-  duration: 2000,
-  origin: "left",
-  distance: "200px",
-  delay: 200,
-});
+function revealElement(selector, duration, origin, distance, delay = 0) {
+  sr.reveal(selector, {
+    duration: duration,
+    origin: origin,
+    distance: distance,
+    delay: delay,
+  });
+}
 
-sr.reveal(".hero .image", {
-  duration: 2000,
-  origin: "right",
-  distance: "200px",
-});
-
-// about
-sr.reveal(".about .intro", {
-  duration: 2000,
-  origin: "right",
-  distance: "200px",
-});
-
-sr.reveal(".about .img", {
-  duration: 2000,
-  origin: "left",
-  distance: "200px",
-  rotate: {
-    x: 60,
-    y: 45,
-  },
-});
-
-/* services */
-sr.reveal(".services .intro", {
-  duration: 2000,
-  origin: "top",
-  distance: "200px",
-});
-
-sr.reveal(".services .img", {
-  duration: 2000,
-  origin: "bottom",
-  distance: "200px",
-});
-
-// portfolio
-sr.reveal(".portfolio .intro", {
-  duration: 2000,
-  origin: "bottom",
-  distance: "200px",
-});
-
-sr.reveal(".portfolio .img", {
-  duration: 2000,
-  origin: "top",
-  distance: "200px",
-});
-
-sr.reveal(".projects .intro", {
-  duration: 2000,
-  origin: "left",
-  distance: "200px",
-});
-
-sr.reveal(".projects .img", {
-  duration: 2000,
-  origin: "right",
-  distance: "200px",
-});
-
-sr.reveal(".Clients .intro", {
-  duration: 2000,
-  origin: "right",
-  distance: "200px",
-});
-
-sr.reveal(".Clients .img", {
-  duration: 2000,
-  origin: "left",
-  distance: "200px",
-});
+// Usage of reveal function
+revealElement(".hero .intro", 2000, "left", "200px");
+revealElement(".hero .image", 2000, "right", "200px");
+revealElement(".section .intro", 2000, "top", "200px");
+revealElement(".section .img", 2000, "bottom", "200px", 200);
